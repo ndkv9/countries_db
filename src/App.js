7f4735449aa817ce0e6f5ba8028730a4a32ce171
Vu Nguyen <vu.nguyen101@gmail.com>
2021-01-28 12:37:03 +0200

    add axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const App = () => {
+	const [countries, setCountries] = useState([])
+	const [name, setName] = useState('')
+
+	useEffect(() => {
+		axios
+			.get('https://restcountries.eu/rest/v2/all')
+			.then(res => setCountries(res.data))
+			.catch(err => console.log(err.message))
+	})
 	return (
 		<div>
 			<h2>Countries DB</h2>
